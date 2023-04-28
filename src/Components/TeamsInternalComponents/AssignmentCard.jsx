d766503dd453a13b57ce5cf4ff908a3bc9331df6
Jitendra Kumar Pandey <84286157+JKP-2001@users.noreply.github.com>
2023-04-29 01:44:53 +0530

    Fix Responsivness Of UI

--- src/Components/TeamsInternalComponents/AssignmentCard.jsx
+++ src/Components/TeamsInternalComponents/AssignmentCard.jsx
@@ -3,30 +3,32 @@ import { Link } from 'react-router-dom'
 import grp_icon from "../.././Images/grp_icon.jpg"
 
 const AssignmentCard = (props) => {
-  const [date,setDate] = useState();
+  const [date, setDate] = useState();
 
-  
-  useEffect(()=>{
-    setDate(new Date());
-  },[])
 
-  
-    return (
-      <Link to={`/assignment/${props.grpId}/${props.postId}`}><div className="card my-[6px] rounded-xl bg-white hover:shadow-xl border-[3px] hover:cursor-pointer" >
-          <div className=" flex item">
-              <div className="grp_image mx-4 pt-[32px]">
-                  <div className="account"><img className="rounded-2xl " src={grp_icon} alt="" /></div>
-              </div>
-              <div className="title py-[10px]">
-                  <h3 className='font-medium text-md md:text-xl break-all pr-2'>{props.title}</h3>
-                  <div className="date mt-1"> <span className="text-[#737373]">{props.grp_name}</span>
-                  <span className='text-[#c4314b] flex'><div></div> {props.date<date?<div className='font-semibold'>Past Due</div>:<div className=''>Due</div>}{", "+props.dueDate}, {props.dueTime}.</span></div>
-              </div>
-  
-          </div>
+  useEffect(() => {
+    let date = new Date().toString();
+    // date = date.toLocaleTimeString('en-GB',{ hour: "2-digit", minute: "2-digit" })
+    setDate(date);
+  }, [])
+
+
+  return (
+    <Link to={`/assignment/${props.grpId}/${props.postId}`}><div className="card my-[6px] rounded-xl bg-white hover:shadow-xl border-[3px] hover:cursor-pointer" >
+      <div className=" flex item">
+        <div className="grp_image mx-4 pt-[32px]">
+          <div className="account"><img className="rounded-2xl " src={grp_icon} alt="" /></div>
+        </div>
+        <div className="title py-[10px]">
+          <h3 className='font-medium text-md md:text-xl break-all pr-2'>{props.title}</h3>
+          <div className="date mt-1"> <span className="text-[#737373]">{props.grp_name}</span>
+            <span className='text-[#c4314b] flex'><div></div> {new Date(props.date) < new Date(date) ? <div className='font-semibold'>Past Due</div> : <div className=''>Due</div>}{", " + props.dueDate}, {props.dueTime}.</span></div>
+        </div>
+
       </div>
-      </Link>
-    ) 
+    </div>
+    </Link>
+  )
 }
 
 export default AssignmentCard
